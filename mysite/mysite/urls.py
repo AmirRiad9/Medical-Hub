94e1d1a13c4962bff9cb87a7d6a9875c50ea09fa
ariad@bu.edu <60357207+AmirRiad9@users.noreply.github.com>
2022-04-19 17:24:49 -0400

    added api description using drf and swagger, added cors for server queries.

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,12 +14,31 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path
 from django.conf.urls import include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.schemas.views import SchemaView
 
-
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Medical Hub API",
+        default_version='v1',
+        description="Welcome to Medhub's API",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    re_path(r'^doc(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('doc/', schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),  # <-- Here
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0),
+         name='schema-redoc'),  # <-- Here
+    path('docs/', SchemaView.as_view()),
     path('admin/', admin.site.urls),
     path('',include('devices.urls')),
     path('chat/', include('chat.urls')),
